4540aa00a2f28b9526103669ac016395dcb38871
Bribb0L <50684429+Bribb0L@users.noreply.github.com>
2023-09-22 05:00:57 -0300

    Change components to include color themes

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import './App.css';
 import GeneralInfo from './components/GeneralInfo';
 import EducationExp from './components/EducationExp';
 import PracticalExp from './components/PracticalExp';
+import Config from './components/Config';
 
 const styles = {
   width: '100%',
   height: '10.5pt',
-  backgroundColor: 'rgb(21, 128, 61)',
   color: 'red'
 };
 
@@ -16,23 +16,27 @@ function App() {
   const states = ['filling', 'filled'];
   const [status, setStatus] = useState(0);
   const [statusId, setStatusId] = useState(0);
+  const [currentColor, setcurrentColor] = useState('#0369a1');
 
   return (
     <>
-      <div style={styles}></div>
+      <div style={{...styles, backgroundColor: currentColor}}></div>
       {status === 0 ? (
         <>
-          <GeneralInfo key='info' status={status} />
-          <EducationExp key='edexp' status={status} />
-          <PracticalExp key='prexp' status={status} />
+          <div className='main-title'><h1>CV-App: Fill the following fields</h1></div>    
+          <GeneralInfo key='info' status={status} currentColor={currentColor} />
+          <EducationExp key='edexp' status={status} currentColor={currentColor} />
+          <PracticalExp key='prexp' status={status} currentColor={currentColor} />
+          <Config key='cfg' status={status} setcurrentColor={setcurrentColor} currentColor={currentColor} />
           <button onClick={() => setStatus(1)}>Create</button>
         </>
       ) : (
         <>
-          <GeneralInfo key='info' status={status} />
+          <GeneralInfo key='info' status={status} currentColor={currentColor} />
           <hr></hr>
-          <EducationExp key='edexp' status={status} />
-          <PracticalExp key='prexp' status={status} />
+          <EducationExp key='edexp' status={status} currentColor={currentColor} />
+          <PracticalExp key='prexp' status={status} currentColor={currentColor} />
+          <Config key='cfg' status={status} setcurrentColor={setcurrentColor} currentColor={currentColor} />
           <button onClick={() => setStatus(0)}>Edit</button>
         </>
       )}
